6d25da298ea06fa79f379a465c8fa369a893e602
John Gabriele <jmg3000@gmail.com>
2013-02-13 23:56:43 -0500

    incremented version string

--- rippledoc.pl
+++ rippledoc.pl
@@ -23,7 +23,7 @@ use File::Find;
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-say "=== Rippledoc, 2013-02-11 ===";
+say "=== Rippledoc, 2013-02-13 ===";
 
 if (! -e 'index.md') {
     die "Couldn't find an index.md file here.\n" .
